aed8ec5bbd5e6b1a83a5b79cd477b926be7473bb
JorgePallolTrigueros <jorgepallol1@gmail.com>
2021-12-13 20:23:57 +0100

    css
    el css del proyecto

---
+++ assets/js/barras.js
@@ -0,0 +1,59 @@
+
+setTimeout(() => {
+
+    $("#pb-angular").animate({
+        width: "70%"
+    }, 2500);
+
+    $("#pb-bootstrap").animate({
+        width: "90%"
+    }, 2500);
+
+    $("#pb-jpa").animate({
+        width: "70%"
+    }, 2500);
+
+    $("#pb-net").animate({
+        width: "68%"
+    }, 2500);
+
+    $("#pb-ionic").animate({
+        width: "70%"
+    }, 2500);
+
+    $("#pb-php").animate({
+        width: "68%"
+    }, 2500); 
+
+
+//
+
+$("#pb-spring").animate({
+    width: "70%"
+}, 2500);
+
+$("#pb-css").animate({
+    width: "90%"
+}, 2500);
+
+$("#pb-django").animate({
+    width: "70%"
+}, 2500);
+
+$("#pb-python").animate({
+    width: "68%"
+}, 2500);
+
+$("#pb-serviface").animate({
+    width: "70%"
+}, 2500);
+
+$("#pb-mysql").animate({
+    width: "68%"
+}, 2500); 
+
+
+
+
+
+}, 1000);
